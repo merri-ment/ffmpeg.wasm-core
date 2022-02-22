954ca92ccd1e87614b7d726cc15c56df880b4b67
Cam <cam@merriment.info>
2022-02-22 16:31:04 +1300

    remove codecs

--- build.sh
+++ build.sh
@@ -17,19 +17,19 @@ run-all() {
     build-zlib
     build-x264
     build-x265
-    build-libvpx
-    build-wavpack
-    build-lame
-    build-fdk-aac
-    build-ogg
-    build-vorbis
-    build-theora
-    build-opus
-    build-libwebp
-    build-freetype2
-    build-fribidi
-    build-harfbuzz
-    build-libass
+    #build-libvpx
+    #build-wavpack
+    #build-lame
+    #build-fdk-aac
+    #build-ogg
+    #build-vorbis
+    #build-theora
+    #build-opus
+    #build-libwebp
+    #build-freetype2
+    #build-fribidi
+    #build-harfbuzz
+    #build-libass
     #build-aom # disabled as it is extremely slow
     configure-ffmpeg
     build-ffmpeg
